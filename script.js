2ce8cb7617be2236f6748bb9524c5564ec5e95b3
Kv-Ramana01 <145438290+Kv-Ramana01@users.noreply.github.com>
2026-04-23 00:13:05 +0530

    Add theme toggle, localStorage tasks, and live clock

--- script.js
+++ script.js
@@ -2,6 +2,30 @@ const taskInput = document.querySelector("#taskInput");
 const addTaskBtn = document.querySelector('#addTaskBtn');
 const taskList = document.querySelector("#taskList");
 
+const clockTime = document.querySelector("#clockTime");
+const clockDate = document.querySelector("#clockDate");
+const clockPeriod = document.querySelector("#clockPeriod");
+
+const themeToggle = document.querySelector("#themeToggle");
+
+let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+const savedTheme = localStorage.getItem("theme");
+
+if (savedTheme === "light") {
+  document.body.classList.add("light-mode");
+  themeToggle.checked = true;
+}
+
+themeToggle.addEventListener("change", () => {
+  document.body.classList.toggle("light-mode");
+
+  const isLight = document.body.classList.contains("light-mode");
+
+  localStorage.setItem("theme", isLight ? "light" : "dark");
+});
+renderTasks();
+
+
 addTaskBtn.addEventListener("click", addTask);
 
 taskInput.addEventListener("keydown", (e) => {
@@ -15,19 +39,66 @@ function addTask() {
 
     if(text === "") return;
 
+    tasks.push(text);
+    saveTasks();
+    renderTasks();
+
+    taskInput.value = "";
+}
+
+function renderTasks() {
+  taskList.innerHTML = "";
+
+  tasks.forEach((task,index) => {
     const li = document.createElement("li");
-    li.textContent = text;
+    li.textContent = task;
 
     const deleteBtn = document.createElement("button");
     deleteBtn.textContent = "X";
     deleteBtn.classList.add("delete-btn");
 
-    deleteBtn.addEventListener("click", () => {
-        li.remove();
+    deleteBtn.addEventListener("click", ()=>{
+      tasks.splice(index,1);
+      saveTasks();
+      renderTasks();
     });
 
     li.appendChild(deleteBtn);
     taskList.appendChild(li);
-
-    taskInput.value = "";
+  });
 }
+
+  
+
+function saveTasks() {
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+
+function updateClock(){
+  const now = new Date();
+
+  let hours = now.getHours();
+  const minutes = String(now.getMinutes()).padStart(2,"0");
+  const seconds = String(now.getSeconds()).padStart(2,"0");
+
+  const period = hours >= 12 ? "PM" : "AM";
+
+  hours = hours % 12;
+  if(hours === 0) hours = 12;
+
+  clockTime.textContent = `${String(hours).padStart(2,"0")}:${minutes}:${seconds}`;
+
+  clockPeriod.textContent = period;
+  // clockTime.textContent = now.toLocaleTimeString();
+  // clockDate.textContent = now.toLocaleDateString();
+
+  clockDate.textContent = now.toLocaleDateString("en-GB", {
+    weekday: "long",
+    day:"numeric",
+    month:"long",
+    year:"numeric"
+  });
+}
+
+updateClock();
+setInterval(updateClock, 1000);
